9cfb1b57086abeb04a11d5cb600eccfdcd7504a2
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2023-10-17 17:46:05 +0700

    Time: 529 ms (95.41%), Space: 29.2 MB (47.32%) - LeetHub

---
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -0,0 +1,4 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        count = collections.Counter(nums)
+        return sum(min(count[num], count[k - num]) for num in count) // 2
