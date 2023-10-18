481198a81a34751028debc8efb1b830e7442c953
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2023-10-18 14:59:56 +0700

    Time: 190 ms (58.86%), Space: 35 MB (50.47%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -7,11 +7,10 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         return self.getGoodNodes(root, float("-inf"))
-        
+
     def getGoodNodes(self, root: TreeNode, newMax: int) -> int:
         if not root:
             return 0
         
         newMax = max(newMax, root.val)
         return (1 if root.val >= newMax else 0) + self.getGoodNodes(root.left, newMax) + self.getGoodNodes(root.right, newMax)
-    
