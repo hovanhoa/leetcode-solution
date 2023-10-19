580c9de8834f705f122c4ffae31bc1f8762c0a80
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2023-10-19 14:00:59 +0700

    Time: 148 ms (65.85%), Space: 16.6 MB (95.13%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,12 +1,10 @@
-class Solution(object):
-    def canPlaceFlowers(self, flowerbed, n):
-        """
-        :type flowerbed: List[int]
-        :type n: int
-        :rtype: bool
-        """
-        for i, flower in enumerate(flowerbed):
-            if flower == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
-                flowerbed[i] = 1
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
                 n -= 1
-        return n <= 0
+                flowerbed[i] = 1
+            if n <= 0:
+                return True
+
+        return False
