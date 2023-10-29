9662b7223b15cf226a7cef647b96089cab7b0fd0
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2023-10-29 21:49:56 +0700

    Time: 589 ms (74.8%), Space: 29.2 MB (57.94%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left = 0 
+        left = 0
         right = len(height) - 1
         maxArea = 0
 
@@ -11,5 +11,5 @@ class Solution:
                 left += 1
             else:
                 right -= 1
-        
+            
         return maxArea
