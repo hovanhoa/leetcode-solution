607b18831170b2e2b4b6cef94037a4e28bce7742
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-03-30 19:18:45 +0700

    Time: 248 ms (59.77%), Space: 13.4 MB (85.84%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -5,20 +5,23 @@ class Solution(object):
         :type word2: str
         :rtype: bool
         """
-        if len(word1)!=len(word2):
+        if len(word1) != len(word2):
             return False
-        d1={}
-        d2={}
+
+        map1={}
+        map2={}
         for i in word1:
-            d1[i]=d1.get(i,0)+1
+            map1[i]=map1.get(i,0)+1
         for i in word2:
-            d2[i]=d2.get(i,0)+1
-        if set(d1.keys())!=set(d2.keys()):
+            map2[i]=map2.get(i,0)+1
+
+        if set(map1.keys()) != set(map2.keys()):
             return False
-        a=list(d1.values())
-        b=list(d2.values())
+
+        a = list(map1.values())
+        b = list(map2.values())
         a.sort()
         b.sort()
-        return a==b
-        
+
+        return a == b
         
