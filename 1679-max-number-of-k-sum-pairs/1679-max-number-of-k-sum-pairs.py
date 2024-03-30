94899bb3582f7351532463ceb385d8ff8e0cc06e
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-03-30 10:37:51 +0700

    Time: 494 ms (91.05%), Space: 21.8 MB (15.33%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -5,15 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        b = {}
+        numsMap = {}
         ans = 0
         for i in nums:
-            if k - i in b and b[k - i] > 0:
+            if k - i in numsMap and numsMap[k - i] > 0:
                 ans += 1
-                b[k - i] -= 1
-            elif i not in b:
-                b[i] = 1
+                numsMap[k - i] -= 1
+            elif i not in numsMap:
+                numsMap[i] = 1
             else:
-                b[i] += 1
-        
+                numsMap[i] += 1
+
         return ans
