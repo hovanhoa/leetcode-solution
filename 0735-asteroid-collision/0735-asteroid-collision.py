f381b82d8eb04238f7a8f97f82c5576459b4bfb8
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-03-31 09:40:34 +0700

    Time: 63 ms (81.19%), Space: 13 MB (13.71%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,16 +1,19 @@
-class Solution:
-    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stack = []
-        
-        for i in asteroids:
-            if i > 0:
-                stack.append(i)
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for a in asteroids:
+            while res and res[-1] > 0 > a:
+                if res[-1] < abs(a):
+                    res.pop()
+                    continue
+                elif res[-1] == abs(a):
+                    res.pop()
+                break
             else:
-                while stack and stack[-1] > 0 and stack[-1] < -i:
-                    stack.pop()
-                if not stack or stack[-1] < 0:
-                    stack.append(i)
-                elif stack and stack[-1] == -i:
-                    stack.pop()
+                res.append(a)
         
-        return stack
+        return res
