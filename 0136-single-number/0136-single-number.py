32e4680606bf1351a0ec9ee6c4f472857810bb1b
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-04-01 11:28:17 +0700

    Time: 97 ms (58.34%), Space: 13.7 MB (91.25%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,3 +1,7 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        return reduce(lambda a,b: a ^ b, nums)
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return reduce(lambda a, b: a ^ b, nums)
