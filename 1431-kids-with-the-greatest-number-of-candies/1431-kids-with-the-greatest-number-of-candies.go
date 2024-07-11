35ce3c195d630c2c769c4d740253d9be6acfb649
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-07-11 18:02:22 +0700

    Time: 2 ms (69.08%), Space: 2.5 MB (98.69%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.go
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.go
@@ -1,18 +1,20 @@
 func kidsWithCandies(candies []int, extraCandies int) []bool {
-    result := make([]bool, len(candies))
-
-    maxVal := candies[0]
-    for _, val := range candies {
-        if val > maxVal {
-            maxVal = val
+    maxCandy := 0
+    for _, candy := range candies {
+        if candy > maxCandy {
+            maxCandy = candy
         }
     }
 
-    for index, val := range candies {
-        if val + extraCandies >= maxVal {
-            result[index] = true
+    res := []bool{}
+
+    for _, candy := range candies {
+        if candy + extraCandies >= maxCandy {
+            res = append(res, true)
+        } else {
+            res = append(res, false)
         }
     }
 
-    return result
+    return res
 }
