2ad7e8d7fae0f21e4f69d5318ab73c1343f9e6c3
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-07-23 09:41:05 +0700

    Time: 2 ms (65.82%), Space: 2.5 MB (100%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.go
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.go
@@ -1,15 +1,19 @@
-func gcdOfStrings(str1 string, str2 string) string {
-    if len(str1) < len(str2) {
-        str1, str2 = str2, str1
-    }
+import (
+    "strings"
+)
 
+func gcdOfStrings(str1 string, str2 string) string {
     if str1 == str2 {
         return str1
     }
 
-    if strings.HasPrefix(str1, str2) {
-        return gcdOfStrings(str2, str1[len(str2):])
+    if len(str2) > len(str1) {
+        str1, str2 = str2, str1
     }
 
-    return ""
+    if str1[:len(str2)] != str2 {
+        return ""
+    }
+
+    return gcdOfStrings(str1[len(str2):], str2)
 }
