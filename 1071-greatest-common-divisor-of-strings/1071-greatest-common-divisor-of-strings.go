04380056a9e1f44b73917d6a5e4bc11f10332cb1
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-08-29 08:45:15 +0700

    Time: 0 ms (100%), Space: 2.6 MB (86.67%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.go
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.go
@@ -1,13 +1,13 @@
 func gcdOfStrings(str1 string, str2 string) string {
     if str1 == str2 {
         return str1
-    }
+    }   
 
     if len(str2) > len(str1) {
         return gcdOfStrings(str2, str1)
     }
 
-    if str2 == str1[:len(str2)] {
+    if str1[:len(str2)] == str2 {
         return gcdOfStrings(str2, str1[len(str2):])
     }
 
