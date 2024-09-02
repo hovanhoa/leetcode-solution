125c15b6f3b3bf24ab7b33c0aff57bd9e7788d98
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-02 15:31:52 +0700

    Time: 85 ms (67.91%), Space: 15.2 MB (5.19%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.go
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.go
@@ -1,16 +1,14 @@
 func maxOperations(nums []int, k int) int {
     kMap := map[int]int{}
-    for _, v := range nums {
-        kMap[v]++
-    }
-
     count := 0
     for _, v := range nums {
         if kMap[k-v] > 0 {
-            kMap[k-v]--
             count++
+            kMap[k-v]--
+        } else {
+            kMap[v]++
         }
     }
 
-    return count/2
+    return count
 }
