203c6d7d6dee4db8e93d3d863e7b86de77ebd245
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-06 09:04:21 +0700

    Time: 23 ms (83.7%), Space: 7.4 MB (48.91%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.go
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.go
@@ -1,17 +1,27 @@
 func findDifference(nums1 []int, nums2 []int) [][]int {
-    result1 := []int{}
-    for _, val := range nums1 {
-        if !slices.Contains(nums2, val) && !slices.Contains(result1, val) {
-            result1 = append(result1, val)
+    s1Map := make(map[int]bool)
+    s2Map := make(map[int]bool)
+
+    for _, v := range nums1 {
+        s1Map[v] = true
+    }
+
+    for _, v := range nums2 {
+        s2Map[v] = true
+    }
+
+    res := make([][]int, 2)
+    for k := range s1Map {
+        if !s2Map[k] {
+            res[0] = append(res[0], k)
         }
     }
 
-    result2 := []int{}
-    for _, val := range nums2 {
-        if !slices.Contains(nums1, val) && !slices.Contains(result2, val) {
-            result2 = append(result2, val)
+    for k := range s2Map {
+        if !s1Map[k] {
+            res[1] = append(res[1], k)
         }
     }
 
-    return [][]int{result1, result2}
+    return res
 }
