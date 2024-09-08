bc42dca02eeb65620da56863560906d3520bc8c2
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-08 17:15:07 +0700

    Time: 3 ms (16.67%), Space: 2.9 MB (100%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.go
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.go
@@ -1,25 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 func splitListToParts(head *ListNode, k int) []*ListNode {
     length := 0
     current := head
-    parts := make([]*ListNode, 0, k)
-
     for current != nil {
         length++
         current = current.Next
     }
 
-    baseSize, extra := length/k, length%k
     current = head
-
-    for i := 0; i < k; i++ {
-        partSize := baseSize
+    base, extra := length/k, length%k
+    res := make([]*ListNode, 0, k)
+    for i := 0; i < k; i ++ {
+        path := base
         if extra > i {
-            partSize++
+            path++
         }
 
         var partHead, partTail *ListNode
-
-        for j := 0; j < partSize; j++ {
+        for j := 0; j < path; j++ {
             if partHead == nil {
                 partHead, partTail = current, current
             } else {
@@ -36,8 +40,9 @@ func splitListToParts(head *ListNode, k int) []*ListNode {
             partTail.Next = nil
         }
 
-        parts = append(parts, partHead)
+        res = append(res, partHead)
+        
     }
 
-    return parts
+    return res
 }
