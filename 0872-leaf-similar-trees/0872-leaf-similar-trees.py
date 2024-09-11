709480b8c2fc881eff1779a216215206ae998db1
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-11 10:46:34 +0700

    Time: 14 ms (74.54%), Space: 11.7 MB (58.25%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -1,18 +1,30 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def getLeafs(root):
-            if not root:
-                return
-            if not root.left and not root.right:
-                yield root.val
-                return
-            yield from getLeafs(root.left)
-            yield from getLeafs(root.right)
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        leaves1, leaves2 = [], []
 
-        return list(getLeafs(root1)) == list(getLeafs(root2))  
+        self.getLeafs(root1, leaves1)
+        self.getLeafs(root2, leaves2)
+
+        return leaves1 == leaves2
+
+    def getLeafs(self, root, leaves):
+        if not root:
+            return
+        
+        if not root.left and not root.right:
+            leaves.append(root.val)
+        
+        self.getLeafs(root.left, leaves)
+        self.getLeafs(root.right, leaves)
+        
