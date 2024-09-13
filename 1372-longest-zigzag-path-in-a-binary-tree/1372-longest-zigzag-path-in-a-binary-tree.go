4abe786d635b02183db72e17870e87ee0ff8ab82
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-13 11:03:31 +0700

    Time: 114 ms (30.81%), Space: 15.8 MB (22.7%) - LeetHub

---
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+
+    return b
+}
+
+func dfs(root *TreeNode, isLeft bool, count int) int {
+    if root == nil {
+        return count
+    }
+
+    if isLeft {
+        return max(dfs(root.Right, false, count + 1), dfs(root.Left, true, 0))
+    }
+
+    return max(dfs(root.Left, true, count + 1), dfs(root.Right, false, 0))
+}
+
+func longestZigZag(root *TreeNode) int {
+    return max(dfs(root.Left, true, 0), dfs(root.Right, false, 0))
+}
