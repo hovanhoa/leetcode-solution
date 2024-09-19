524dc94486bc2f1ede41ecddefb5018d3ff599ab
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-19 09:35:46 +0700

    Time: 1 ms (75.83%), Space: 2.1 MB (50.47%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.go
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.go
@@ -1,20 +1,22 @@
 func tribonacci(n int) int {
-    if n == 0 {
-        return 0
-    } else if n < 3 {
+    if n == 0 || n == 1 {
+        return n
+    }
+
+    if n == 2 {
         return 1
     }
 
     t0 := 0
     t1 := 1
     t2 := 1
-    
-    for i := 0; i < (n - 2); i++ {
-        newT := t0 + t1 + t2
+    sum := 0
+    for i := 3; i <= n; i++ {
+        sum = t0 + t1 + t2
         t0 = t1
         t1 = t2
-        t2 = newT
+        t2 = sum
     }
-
-    return t2
+    
+    return sum
 }
