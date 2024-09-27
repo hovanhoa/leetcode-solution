238aeacdbda954e64b41412de24a1adf2b1e857b
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-27 09:24:23 +0700

    Time: 1 ms (72.45%), Space: 2.3 MB (97.8%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.go
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -1,32 +1,17 @@
 func mergeAlternately(word1 string, word2 string) string {
-    ans := make([]byte, len(word1) + len(word2))
-    count := 0
     i, j := 0, 0
-    for i < len(word1) && j < len(word2) {
+    result := []byte{}
+    for i < len(word1) || j < len(word2) {
         if i < len(word1) {
-            ans[count] = word1[i]
-            i++
-            count++
+            result = append(result, word1[i])
+            i += 1
         }
 
         if j < len(word2) {
-            ans[count] = word2[j]
-            j++
-            count++
+            result = append(result, word2[j])
+            j += 1
         }
     }
 
-    for i < len(word1) {
-        ans[count] = word1[i]
-        i++
-        count++
-    }
-
-    for j < len(word2) {
-        ans[count] = word2[j]
-        j++
-        count++
-    }
-
-    return string(ans)
+    return string(result)
 }
