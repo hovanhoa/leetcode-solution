bd9e5f92e1003adce9b0274b27f23f336925dfc6
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-27 10:23:34 +0700

    Time: 18 ms (61.79%), Space: 6.7 MB (91.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.go
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -1,9 +1,14 @@
 func moveZeroes(nums []int)  {
-    idx := 0
-    for i := 0; i < len(nums); i ++ {
-        if nums[i] != 0 {
-            nums[i], nums[idx] = nums[idx], nums[i]
-            idx ++
+    count := 0
+    for _, v := range nums {
+        if v != 0 {
+            nums[count] = v
+            count++
         }
+    }
+
+    for count < len(nums) {
+        nums[count] = 0
+        count++
     }
 }
