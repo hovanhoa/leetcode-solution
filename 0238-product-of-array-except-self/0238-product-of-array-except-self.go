16dbd7c1b3c2ef72978b5f5116b26af8362a999e
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-28 10:52:55 +0700

    Time: 37 ms (67.87%), Space: 10.4 MB (12.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -1,25 +1,25 @@
 func productExceptSelf(nums []int) []int {
-    leftArr := make([]int, len(nums))
-    rightArr := make([]int, len(nums))
-    for i := 0; i < len(nums); i ++ {
+    left, right := make([]int, len(nums)), make([]int, len(nums))
+    for i := 0; i < len(nums); i++ {
         if i == 0 {
-            leftArr[i] = 1
+            left[i] = 1
         } else {
-            leftArr[i] = leftArr[i-1] * nums[i-1]
+            left[i] = left[i-1] * nums[i-1]
         }
     }
 
-    for i := len(nums) - 1; i >= 0; i -- {
+    for i := len(nums) - 1; i >= 0; i-- {
         if i == len(nums) - 1 {
-            rightArr[i] = 1
+            right[i] = 1
         } else {
-            rightArr[i] = rightArr[i+1] * nums[i+1]
+            right[i] = right[i+1] * nums[i+1]
         }
     }
 
-    for i := 0; i < len(nums); i ++ {
-        leftArr[i] = leftArr[i] * rightArr[i]
+    ans := make([]int, len(nums))
+    for i := range ans {
+        ans[i] = left[i] * right[i] 
     }
 
-    return leftArr
+    return ans
 }
