ffedb936bdaaa77deac911cea9a00f9cdac33dd7
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-09-28 10:40:32 +0700

    Time: 2 ms (73.3%), Space: 3.1 MB (87.12%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.go
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.go
@@ -1,11 +1,5 @@
 func reverseWords(s string) string {
     words := strings.Fields(s)
-    l, r := 0, len(words) - 1
-    for l < r {
-        words[l], words[r] = words[r], words[l]
-        l++
-        r--
-    }
-
+    slices.Reverse(words)
     return strings.Join(words, " ")
 }
