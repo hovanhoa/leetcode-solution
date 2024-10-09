f2563bf8f2ba4281574551b128b8439b9428fac1
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-10-09 10:52:39 +0700

    Time: 7 ms (74.73%), Space: 7.3 MB (65.32%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.go
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.go
@@ -6,21 +6,21 @@
  *     Right *TreeNode
  * }
  */
-
+ 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-  if root == nil || root == p || root == q {
-    return root
-  }
+    if root == nil || root == p || root == q {
+        return root
+    }
 
-  left := lowestCommonAncestor(root.Left, p, q)
-  right := lowestCommonAncestor(root.Right, p, q)
-  if left != nil && right != nil {
-    return root
-  }
+    left := lowestCommonAncestor(root.Left, p, q)
+    right := lowestCommonAncestor(root.Right, p, q)
+    if left != nil && right != nil {
+        return root
+    }
 
-  if left != nil {
-    return left
-  }
+    if left != nil {
+        return left
+    }
 
-  return right
+    return right
 }
