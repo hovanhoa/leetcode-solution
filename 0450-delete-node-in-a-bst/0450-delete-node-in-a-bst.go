2a37095de0a8ad58c8689e391d6c1062a35b5657
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-10-10 13:42:55 +0700

    Time: 8 ms (86.15%), Space: 6.8 MB (66.76%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func deleteNode(root *TreeNode, key int) *TreeNode {
+    if root == nil {
+        return nil
+    }   
+
+    if root.Val < key {
+        root.Right = deleteNode(root.Right, key)
+    } else if root.Val > key {
+        root.Left = deleteNode(root.Left, key)
+    } else if root.Left == nil {
+        return root.Right
+    } else if root.Right == nil {
+        return root.Left
+    } else {
+        node := root.Right
+        for node.Left != nil {
+            node = node.Left
+        }
+
+        root.Val = node.Val
+        root.Right = deleteNode(root.Right, node.Val)
+    }
+
+    return root
+}
