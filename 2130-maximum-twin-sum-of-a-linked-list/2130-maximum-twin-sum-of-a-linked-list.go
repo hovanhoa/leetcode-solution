1160acae5d603fe91681465c4560a6ee5ebc8925
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-10-11 11:42:16 +0700

    Time: 164 ms (48.46%), Space: 12.9 MB (34.92%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.go
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.go
@@ -6,21 +6,19 @@
  * }
  */
 func pairSum(head *ListNode) int {
+    node := head
     arr := []int{}
-    for head != nil {
-        arr = append(arr, head.Val)
-        head = head.Next
+    for node != nil {
+        arr = append(arr, node.Val)
+        node = node.Next
     }
 
-    l, r := 0, len(arr) - 1
-    max := 0
-    for l < r {
-        if arr[l] + arr[r] > max {
-            max = arr[l] + arr[r]
+    ans := 0
+    for i := 0; i < len(arr)/2; i++ {
+        if arr[i] + arr[len(arr)-1-i] > ans {
+            ans = arr[i] + arr[len(arr)-1-i]
         }
-        l++
-        r--
     }
 
-    return max
+    return ans
 }
