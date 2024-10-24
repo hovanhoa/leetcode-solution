f5f05b369df6f92e81cfa0c5170ec8cfcaa6a90c
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-10-24 10:41:56 +0700

    Time: 0 ms (100%), Space: 16.7 MB (13.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
-            return max(nums)
-        memorize = [0]*len(nums)
-        memorize[-1] = nums[-1]
-        memorize[-2] = nums[-2]
-        for i in range(len(nums)-3, -1, -1):
-            memorize[i] = max(nums[i], nums[i] + max(memorize[i+2:]))
-        return max(memorize[0], memorize[1])
+        if len(nums) == 1:
+            return nums[0]
+
+        dp = [0 for i in range(len(nums))]
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+
+        return dp[-1]
+        
