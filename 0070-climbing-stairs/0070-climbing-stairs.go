68da60ab4f01cfe90a4f69b607a3761f45a71f9d
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-10-24 16:07:19 +0700

    Time: 0 ms (100%), Space: 3.9 MB (8.85%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.go
@@ -0,0 +1,13 @@
+func climbStairs(n int) int {
+    if n == 1 {
+        return 1
+    }
+    
+    dp := make([]int, n)
+    dp[0], dp[1] = 1, 2
+    for i := 2; i < len(dp); i++ {
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+
+    return dp[n-1]
+}
