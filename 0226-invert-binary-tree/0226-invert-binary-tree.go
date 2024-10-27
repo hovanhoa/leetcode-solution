1c2ce436e83c4409e3e135e91495bef0bf8faf6f
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-10-27 15:51:27 +0700

    Time: 0 ms (100%), Space: 3.9 MB (11.14%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    temp := root.Left
+    root.Left = root.Right
+    root.Right = temp
+
+    invertTree(root.Left)
+    invertTree(root.Right)
+
+    return root
+}
