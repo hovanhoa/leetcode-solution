6dde2060c40ded28bb329345ba316509995736da
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-10-27 16:02:18 +0700

    Time: 0 ms (100%), Space: 12.8 MB (6.1%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.go
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.go
@@ -1,8 +1,7 @@
 func replaceElements(arr []int) []int {
     ans := make([]int, len(arr))
-    ans[len(arr)-1] = -1
-    max := arr[len(arr)-1]
-    for i := len(arr)-2; i >= 0; i-- {
+    max := -1
+    for i := len(arr)-1; i >= 0; i-- {
         ans[i] = max
 
         if max < arr[i] {
