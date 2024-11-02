6c9e8f07dd40131ce7927336d77fe9bb03d7af89
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-11-02 17:13:51 +0700

    Time: 0 ms (100%), Space: 3.9 MB (5.7%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.go
+++ 0263-ugly-number/0263-ugly-number.go
@@ -2,18 +2,12 @@ func isUgly(n int) bool {
     if n < 1 {
         return false
     }
-    
-    for n != 1 {
-        if n % 2 == 0 {
-            n = n/2
-        } else if n % 3 == 0 {
-            n = n/3
-        } else if n % 5 == 0 {
-            n = n/5
-        } else {
-            return false
+
+    for _, v := range []int{2, 3, 5} {
+        for n % v == 0 {
+            n /= v
         }
     }
 
-    return true
+    return n == 1 
 }
