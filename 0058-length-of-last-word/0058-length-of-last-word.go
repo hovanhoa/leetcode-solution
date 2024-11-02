3f05255c5351ea6c379fc51dd3271f3c2e6c8e5f
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-11-02 17:23:20 +0700

    Time: 0 ms (100%), Space: 4 MB (14.37%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.go
+++ 0058-length-of-last-word/0058-length-of-last-word.go
@@ -1,13 +1,12 @@
 func lengthOfLastWord(s string) int {
-    isStart := false
-    ans := 0
-    for i := len(s) - 1; i >= 0; i-- {
-        if s[i] != ' ' {
-            ans += 1
-            isStart = true
-        } else if isStart {
-            return ans
-        }
+    i, ans := len(s) - 1, 0
+    for s[i] == ' ' {
+        i -= 1
+    }
+
+    for i >= 0 && s[i] != ' ' {
+        ans += 1
+        i -= 1
     }
 
     return ans
