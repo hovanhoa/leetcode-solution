ea65f1610e757d21ff51469bf8d8068f10c7a689
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-11-03 14:23:50 +0700

    Time: 0 ms (100%), Space: 4.6 MB (30.25%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.go
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.go
@@ -2,12 +2,12 @@ func minCostClimbingStairs(cost []int) int {
     cost = append(cost, 0)
 
     for i := 2; i < len(cost); i++ {
-        if cost[i-1] > cost[i-2] {
-            cost[i] += cost[i-2]
-        } else {
+        if cost[i-1] < cost[i-2] {
             cost[i] += cost[i-1]
+        } else {
+            cost[i] += cost[i-2]
         }
     }
 
-    return cost[len(cost)-1] 
+    return cost[len(cost) - 1]
 }
