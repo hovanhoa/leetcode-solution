4432b40fa9cc4260767138e25ac65901af29f635
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-11-06 11:16:54 +0700

    Time: 0 ms (100%), Space: 8.5 MB (5.01%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.go
+++ 0605-can-place-flowers/0605-can-place-flowers.go
@@ -1,10 +1,8 @@
 func canPlaceFlowers(flowerbed []int, n int) bool {
     for i := 0; i < len(flowerbed); i++ {
-        if flowerbed[i] == 0 {
-            if (i == 0 || flowerbed[i-1] == 0) && (i == len(flowerbed) - 1 || flowerbed[i+1] == 0) {
-                n--
-                flowerbed[i] = 1
-            } 
+        if flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == len(flowerbed) - 1 || flowerbed[i+1] == 0) {
+            flowerbed[i] = 1
+            n -= 1
         }
     }
 
