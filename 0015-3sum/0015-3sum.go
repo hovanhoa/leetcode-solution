09e2d8e4f7e653615c0945776bd4167b3b57ea2e
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-11-19 10:54:45 +0700

    Time: 19 ms (85.18%), Space: 9.2 MB (52.72%) - LeetHub

---
+++ 0015-3sum/0015-3sum.go
@@ -0,0 +1,30 @@
+func threeSum(nums []int) [][]int {
+    ans := [][]int{}
+    sort.Slice(nums, func(i, j int) bool {
+        return nums[i] < nums[j]
+    })
+
+    for i := 0; i < len(nums) - 2; i++ {
+        if i > 0 && nums[i] == nums[i-1] {
+            continue
+        }
+
+        l, r := i + 1, len(nums) - 1
+        for l < r {
+            total := nums[i] + nums[l] + nums[r]
+            if total < 0 {
+                l += 1
+            } else if total > 0 {
+                r -= 1
+            } else {
+                ans = append(ans, []int{nums[i], nums[l], nums[r]})
+                l += 1
+                for nums[l] == nums[l-1] && l <r {
+                    l += 1
+                }
+            }
+        }
+    }
+
+    return ans
+}
