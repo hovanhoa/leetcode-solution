7e3995fd7d3bda1df1763ff57eb511b26bdb6621
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-11-25 13:52:06 +0700

    Time: 11 ms (74.06%), Space: 8.2 MB (63.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.go
+++ 0322-coin-change/0322-coin-change.go
@@ -2,16 +2,15 @@ func coinChange(coins []int, amount int) int {
     dp := make([]int, amount + 1)
     for i := range dp {
         dp[i] = amount + 1
-    }
-    dp[0] = 0
+    }  
 
-    for n := 1; n < amount + 1; n++ {
+    dp[0] = 0 
+    for i := 1; i < amount + 1; i++ {
         for _, c := range coins {
-            if n >= c {
-                dp[n] = min(dp[n], 1 + dp[n-c])
+            if i >= c {
+                dp[i] = min(dp[i], dp[i-c] + 1)
             }
         }
-
     }
 
     if dp[amount] != amount + 1 {
@@ -19,12 +18,4 @@ func coinChange(coins []int, amount int) int {
     }
 
     return -1
-}
-
-func min(a, b int) int {
-    if a < b {
-        return a
-    }
-
-    return b
 }
