8fd5020b78cb039ea2690ec40e8ec9894ca263e6
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-11-26 10:23:50 +0700

    Time: 0 ms (100%), Space: 6.1 MB (24.19%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.go
+++ 0002-add-two-numbers/0002-add-two-numbers.go
@@ -10,7 +10,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
     cur := ans
     carry := 0
     for l1 != nil || l2 != nil || carry > 0 {
-        sum := carry
+        sum := 0
         if l1 != nil {
             sum += l1.Val
             l1 = l1.Next
@@ -21,10 +21,12 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
             l2 = l2.Next
         }
 
-        carry = sum / 10
-        sum = sum % 10
+        sum += carry
+        carry = sum/10
+        
+
         cur.Next = &ListNode{
-            Val: sum,
+            Val: sum%10,
         }
         cur = cur.Next
     }
