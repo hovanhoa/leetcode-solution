5f5257c94c2a14f85b8edceb1cbfd89475bf0e53
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-11-26 22:32:39 +0700

    Time: 25 ms (50.61%), Space: 5.4 MB (15.82%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.go
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.go
@@ -4,7 +4,7 @@ func lengthOfLIS(nums []int) int {
         dp[i] = 1
     }
 
-    for i := len(nums) - 1; i >=0; i-- {
+    for i := len(nums) - 1; i >= 0; i-- {
         for j := i + 1; j < len(nums); j++ {
             if nums[i] < nums[j] {
                 dp[i] = max(dp[i], dp[j] + 1)
