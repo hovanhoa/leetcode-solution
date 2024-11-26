a4568c61e0bba9398215887b2e12baa6dc4bbdb4
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-11-26 22:38:25 +0700

    Time: 0 ms (100%), Space: 5.3 MB (22.55%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.go
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.go
@@ -1,5 +1,5 @@
 func maxProduct(nums []int) int {
-    ans := max(nums...)
+    ans := slices.Max(nums)
     curMin, curMax := 1, 1
     for _, v := range nums {
         if v == 0 {
@@ -7,33 +7,11 @@ func maxProduct(nums []int) int {
             continue
         }
 
-        tmp := curMax * v
-        curMax = max(curMax * v, curMin * v, v)
-        curMin = min(tmp, curMin * v, v)
-        ans = max(ans, curMax)
+        tmp := curMin
+        curMin = min(curMin * v, curMax * v, v)
+        curMax = max(tmp * v, curMax * v, v)
+        ans = max(curMax, ans)        
     }
 
     return ans
-}
-
-func max(nums ...int) int {
-    max := nums[0]
-    for _, v := range nums {
-        if v > max {
-            max = v
-        }
-    }
-
-    return max
-}
-
-func min(nums ...int) int {
-    min := nums[0]
-    for _, v := range nums {
-        if v < min {
-            min = v
-        }
-    }
-
-    return min
 }
