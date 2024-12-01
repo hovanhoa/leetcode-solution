4f5783edf518b647a6fbc33892afada1ae4efa78
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-12-01 15:44:47 +0700

    Time: 0 ms (100%), Space: 4.8 MB (16.3%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.go
+++ 0022-generate-parentheses/0022-generate-parentheses.go
@@ -1,8 +1,9 @@
 func generateParenthesis(n int) []string {
     ans := []string{}
+    
     var backtracking func(cur string, open int, close int)
     backtracking = func(cur string, open int, close int) {
-        if len(cur) == n * 2 {
+        if len(cur) == 2 * n {
             ans = append(ans, cur)
             return
         }
