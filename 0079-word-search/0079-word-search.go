f7bbbf54d5640e42213e9adeaa43b2047457b1a7
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-12-05 22:43:56 +0700

    Time: 656 ms (12.33%), Space: 4.3 MB (17.85%) - LeetHub

--- 0079-word-search/0079-word-search.go
+++ 0079-word-search/0079-word-search.go
@@ -1,21 +1,21 @@
 func exist(board [][]byte, word string) bool {
+    lenRow, lenCol := len(board), len(board[0])
     mMap := map[[2]int]bool{}
-    row, col := len(board), len(board[0])
-
+    
     var dfs func(i, j, l int) bool
     dfs = func(i, j, l int) bool {
         if l == len(word) {
             return true
         }
 
-        if i < 0 || j < 0 || i >= row || j >= col || board[i][j] != word[l] || mMap[[2]int{i, j}] {
+        if i < 0 || j < 0 || i == lenRow || j == lenCol || mMap[[2]int{i, j}]|| board[i][j] != word[l] {
             return false
         }
 
         mMap[[2]int{i, j}] = true
         res := dfs(i + 1, j, l + 1) || dfs(i - 1, j, l + 1) || dfs(i, j + 1, l + 1) || dfs(i, j - 1, l + 1)
-
         mMap[[2]int{i, j}] = false
+
         return res
     }
 
