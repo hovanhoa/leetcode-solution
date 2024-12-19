18e80580874757179008f103694764fb8e285910
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-12-19 10:45:12 +0700

    Time: 18 ms (38.48%), Space: 5.4 MB (47.7%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.go
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.go
@@ -2,18 +2,18 @@ func characterReplacement(s string, k int) int {
     ans := 0
     m := map[byte]int{}
 
-    l, maxf := 0, 0
+    l, maxM := 0, 0
     for r := 0; r < len(s); r++ {
         m[s[r]] += 1
-        maxf = max(maxf, m[s[r]])
-        
-        for (r - l + 1) - maxf > k {
+        maxM = max(maxM, m[s[r]])
+
+        for (r - l + 1) - maxM > k {
             m[s[l]] -= 1
             l += 1
         }
 
         ans = max(ans, r - l + 1)
-    }   
+    }
 
     return ans
 }
