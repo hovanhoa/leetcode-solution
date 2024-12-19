0565e06de11ff4daa88b19e9ef7c0e4b03d82ba7
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-12-19 11:12:36 +0700

    Time: 3 ms (58.73%), Space: 4.8 MB (35.21%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+
+func dfs(node *Node, visited map[*Node]*Node) *Node {
+    if node == nil {
+        return nil
+    }
+
+    if val, ok := visited[node]; ok {
+        return val
+    }
+
+    visited[node] = &Node{Val: node.Val}
+    for _, nei := range node.Neighbors {
+        visited[node].Neighbors = append(visited[node].Neighbors, dfs(nei, visited))
+    }
+
+    return visited[node]
+}
+
+func cloneGraph(node *Node) *Node {
+    visited := make(map[*Node]*Node)
+    return dfs(node, visited)
+}
