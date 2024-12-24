f83b951c30107e0ed7add19ad7d52db0c5fadbd2
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-12-24 13:50:02 +0700

    Time: 0 ms (100%), Space: 4.1 MB (17.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.go
+++ 0198-house-robber/0198-house-robber.go
@@ -1,21 +1,14 @@
 func rob(nums []int) int {
-    if len(nums) == 1 {
-        return nums[0]
-    }
-
     dp := make([]int, len(nums))
-    dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-    for i := 2; i < len(nums); i++ {
-        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+    dp[0] = nums[0]
+    
+    for i := 1; i < len(nums); i++ {
+        if i == 1 {
+            dp[i] = max(nums[1], dp[0])
+        } else {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        }
     }
 
-    return dp[len(nums)-1]
-}
-
-func max(a, b int) int {
-    if a > b {
-        return a
-    }
-
-    return b
+    return dp[len(dp)-1]
 }
