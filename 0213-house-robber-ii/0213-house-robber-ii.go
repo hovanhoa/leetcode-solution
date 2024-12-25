c01d12044967c6726b42b1ac2a2a3b33506cf39f
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-12-25 08:35:53 +0700

    Time: 0 ms (100%), Space: 4 MB (17.62%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.go
+++ 0213-house-robber-ii/0213-house-robber-ii.go
@@ -2,19 +2,19 @@ func rob(nums []int) int {
     if len(nums) == 1 {
         return nums[0]
     }
-    
+
     return max(findRob(nums[1:]), findRob(nums[:len(nums)-1]))
 }
 
 func findRob(nums []int) int {
+    if len(nums) == 1 {
+        return nums[0]
+    }
+
     dp := make([]int, len(nums))
-    dp[0] = nums[0]
-    for i := 1; i < len(nums); i++ {
-        if i == 1 {
-            dp[i] = max(dp[0], nums[i])
-        } else {
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
-        }
+    dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+    for i := 2; i < len(nums); i++ {
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
     }
 
     return dp[len(nums)-1]
