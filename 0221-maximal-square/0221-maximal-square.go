b470fc3d9336fcfdf7d65efad1e3eee6c86534b0
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-12-28 10:55:47 +0700

    Time: 0 ms (100%), Space: 8.5 MB (81.97%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.go
+++ 0221-maximal-square/0221-maximal-square.go
@@ -1,18 +1,17 @@
 func maximalSquare(matrix [][]byte) int {
-    m, n := len(matrix), len(matrix[0])
-    dp := make([][]int, m)
+    dp := make([][]int, len(matrix))
     for i := range dp {
-        dp[i] = make([]int, n)
+        dp[i] = make([]int, len(matrix[0]))
     }
 
     ans := 0
     for i := range matrix {
-        for j := range matrix[i] {
+        for j := range matrix[0] {
             if matrix[i][j] == '1' {
                 left, top, diag := 0, 0, 0
                 if i > 0 {
                     left = dp[i-1][j]
-                } 
+                }
 
                 if j > 0 {
                     top = dp[i][j-1]
