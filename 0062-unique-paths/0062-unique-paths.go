862a7b328c975f50f60275b36aebb13aa068e2d6
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2024-12-30 11:13:34 +0700

    Time: 0 ms (100%), Space: 4 MB (68.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.go
+++ 0062-unique-paths/0062-unique-paths.go
@@ -2,14 +2,14 @@ func uniquePaths(m int, n int) int {
     dp := make([][]int, m)
     for i := range dp {
         dp[i] = make([]int, n)
-    }
+    } 
 
     for i := range dp {
         for j := range dp[i] {
             if i == 0 || j == 0 {
                 dp[i][j] = 1
             } else {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j]
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
             }
         }
     }
