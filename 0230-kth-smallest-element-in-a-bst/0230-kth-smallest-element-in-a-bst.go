83ae3074b180375ea0ef2456d0eacb756f4b893d
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-01-03 10:32:11 +0700

    Time: 0 ms (100%), Space: 8.5 MB (22.48%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+    n := 0
+    stack := []*TreeNode{}
+    cur := root
+
+    for cur != nil || len(stack) > 0 {
+        for cur != nil {
+            stack = append(stack, cur)
+            cur = cur.Left
+        }
+
+        cur = stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        n += 1
+        if n == k {
+            return cur.Val
+        }
+
+        cur = cur.Right
+    }
+
+    return -1
+}
