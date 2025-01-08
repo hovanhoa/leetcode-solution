36bd0652d61e752b24daf713de125b45bef8d0ee
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-01-08 07:52:54 +0700

    Time: 0 ms (100%), Space: 4.1 MB (90.77%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.go
@@ -0,0 +1,29 @@
+func multiply(num1 string, num2 string) string {
+    if num1 == "0" || num2 == "0" {
+        return "0"
+    }
+
+    n1, n2 := num1, num2
+    ans := make([]byte, len(n1) + len(n2))
+    for i := len(n1) - 1; i >= 0; i-- {
+        for j := len(n2) - 1; j >= 0; j-- {
+            v1 := n1[i] - '0'
+            v2 := n2[j] - '0'
+            ans[i+j+1] += (v1 * v2)
+            if ans[i+j+1] >= 10 {
+                ans[i+j] += (ans[i+j+1]/10)
+                ans[i+j+1] %= 10
+            }
+        }
+    }
+
+    if ans[0] == 0 {
+        ans = ans[1:]
+    }
+
+    for i := range ans {
+        ans[i] += '0'
+    }
+
+    return string(ans)
+}
