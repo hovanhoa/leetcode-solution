911a9ffecff34630fddf3316532550abb9cca74c
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-01-17 13:49:28 +0700

    Time: 30 ms (38.09%), Space: 8.7 MB (99.91%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -8,22 +8,23 @@ func threeSum(nums []int) [][]int {
         if i > 0 && nums[i] == nums[i-1] {
             continue
         }
- 
+
         l, r := i + 1, len(nums) - 1
         for l < r {
-            total := nums[i] + nums[l] + nums[r]
-            if total < 0 {
-                l += 1
-            } else if total > 0 {
-                r -= 1
-            } else {
+            sum := nums[i] + nums[l] + nums[r]
+            if sum == 0 {
                 ans = append(ans, []int{nums[i], nums[l], nums[r]})
                 l += 1
                 for l < r && nums[l] == nums[l-1] {
                     l += 1
                 }
+            } else if sum < 0 {
+                l += 1
+            } else {
+                r -= 1
             }
         }
+
     }
 
     return ans
