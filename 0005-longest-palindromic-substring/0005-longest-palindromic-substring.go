60838083e3376008278a1a6982a3978b69a3e456
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-01-18 08:23:50 +0700

    Time: 4 ms (53.01%), Space: 4.4 MB (61.51%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -2,7 +2,7 @@ func longestPalindrome(s string) string {
     ans := ""
     for i := 0; i < len(s); i++ {
         l, r := i, i
-        for l >= 0 && r <= len(s) - 1 && s[l] == s[r] {
+        for l >= 0 && r < len(s) && s[l] == s[r] {
             if len(ans) < r - l + 1 {
                 ans = s[l:r+1]
             }
@@ -11,14 +11,14 @@ func longestPalindrome(s string) string {
         }
 
         l, r = i, i + 1
-        for l >= 0 && r <= len(s) - 1 && s[l] == s[r] {
+        for l >= 0 && r < len(s) && s[l] == s[r] {
             if len(ans) < r - l + 1 {
                 ans = s[l:r+1]
             }
 
             l, r = l - 1, r + 1
         }
-    }
+    }   
 
     return ans
 }
