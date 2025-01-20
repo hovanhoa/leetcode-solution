6346a016a3a2024cd94aa27e426de6fac7b81a6f
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-01-20 11:20:23 +0700

    Time: 0 ms (100%), Space: 5.2 MB (13.27%) - LeetHub

--- 0046-permutations/0046-permutations.go
+++ 0046-permutations/0046-permutations.go
@@ -4,11 +4,11 @@ func permute(nums []int) [][]int {
         return [][]int{nums}
     }
 
-    for i := 0; i < len(nums); i++ {
+    for i := range nums {
         newNums := append(append([]int{}, nums[:i]...), nums[i+1:]...)
         perms := permute(newNums)
-        for j := range perms {
-            ans = append(ans, append(perms[j], nums[i]))
+        for _, v := range perms {
+            ans = append(ans, append(v, nums[i]))
         }
     }
 
