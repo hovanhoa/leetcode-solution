343690508a0b92af94d6b234f5e498dfe6e598cd
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-02-09 22:10:17 +0700

    Time: 5 ms (40.6%), Space: 14.3 MB (21.37%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.go
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.go
@@ -1,19 +1,11 @@
 func maxProfit(prices []int, fee int) int {
     dp := make([][2]int, len(prices))
-    dp[0][1] = -prices[0]
-    
+    dp[0][1] = - prices[0]
+
     for i := 1; i < len(prices); i++ {
         dp[i][0] = max(dp[i-1][0], prices[i] + dp[i-1][1] - fee)
         dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
     }
-    
+
     return dp[len(prices) - 1][0]
-}
-
-func max(a, b int) int {
-    if a > b {
-        return a
-    }
-
-    return b
 }
