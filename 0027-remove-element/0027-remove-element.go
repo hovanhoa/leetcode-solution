f3e4cefac57ff015a2fa63236ca4a2ba2ee2439a
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-03-01 14:01:56 +0700

    Time: 0 ms (100%), Space: 4.1 MB (68.95%) - LeetHub

--- 0027-remove-element/0027-remove-element.go
+++ 0027-remove-element/0027-remove-element.go
@@ -1,11 +1,15 @@
 func removeElement(nums []int, val int) int {
-    ans := 0
-    for i := 0; i < len(nums); i++ {
-        if nums[i] != val {
-            nums[ans] = nums[i]
-            ans += 1
+    res := 0
+    l, r := 0, 0
+    for r < len(nums) {
+        if nums[r] != val {
+            nums[l] = nums[r]
+            l += 1
+            res += 1
         }
+
+        r += 1
     }
 
-    return ans
+    return res
 }
