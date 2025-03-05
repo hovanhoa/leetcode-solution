a48ff538618b2204b12bb6a53071f960ca64f8f7
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-03-05 22:38:59 +0700

    Time: 4 ms (11.21%), Space: 8.6 MB (56.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -1,12 +1,17 @@
 func majorityElement(nums []int) int {
-    ans, max, m := 0, 0, map[int]int{}
+    m := map[int]int{}
     for _, v := range nums {
         m[v] += 1
-        if m[v] > max {
-            max = m[v]
-            ans = v
+    }
+
+    max := 0
+    res := 0
+    for k, v := range m {
+        if v > max {
+            max = v
+            res = k
         }
     }
 
-    return ans
+    return res
 }
