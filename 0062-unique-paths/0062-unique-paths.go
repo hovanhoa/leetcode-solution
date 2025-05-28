b55238ba8b45d7aa801eceabbb29febbea6a2870
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-05-28 20:53:22 +0700

    Time: 0 ms (100%), Space: 4.1 MB (34.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.go
+++ 0062-unique-paths/0062-unique-paths.go
@@ -1,18 +1,22 @@
 func uniquePaths(m int, n int) int {
-    dp := make([][]int, m)
-    for i := range dp {
-        dp[i] = make([]int, n)
+    arr := make([][]int, m)
+    for i := range arr {
+        arr[i] = make([]int, n)
+    }
+    
+    for i := range arr {
+        arr[i][0] = 1
     }
 
-    for i := range dp {
-        for j := range dp[i] {
-            if i == 0 || j == 0 {
-                dp[i][j] = 1
-            } else {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-            }
+    for i := range arr[0] {
+        arr[0][i] = 1
+    }
+
+    for i := 1; i < m; i++ {
+        for j := 1; j < n; j++ {
+            arr[i][j] = arr[i-1][j] + arr[i][j-1]
         }
     }
 
-    return dp[m-1][n-1]
+    return arr[m-1][n-1]
 }
