e760e7604cce6ab6a288f57e18f8fc3a7fb812c3
Hồ Văn Hòa <56647826+hovanhoa@users.noreply.github.com>
2025-05-28 21:47:04 +0700

    Time: 0 ms (100%), Space: 5.3 MB (41.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.go
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -1,24 +1,25 @@
 func isPalindrome(s string) bool {
-    l, r := 0, len(s) - 1
-    for l < r { 
-        for l < r && !isAlpha(s[l]) {
+    res := strings.ToLower(s)
+    str := []byte{}
+    for i := range res {
+        if isAlphaByte(res[i]) {
+            str = append(str, res[i])
+        }
+    }
+
+    l, r := 0, len(str) - 1
+    for l < r {
+        if str[l] == str[r] {
             l += 1
-        }
-
-        for l < r && !isAlpha(s[r]) {
             r -= 1
-        }
-
-        if strings.ToLower(string(s[l])) != strings.ToLower(string(s[r])) {
+        } else {
             return false
         }
-
-        l, r = l + 1, r - 1
     }
-    
+
     return true
 }
 
-func isAlpha(a byte) bool {
-    return unicode.IsLetter(rune(a)) || unicode.IsNumber(rune(a))
+func isAlphaByte(b byte) bool {
+    return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9')
 }
