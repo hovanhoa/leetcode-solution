50dc9a2f134318c77dff45661e4a5833ac1d7284
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-04-05 15:23:55 +0700

    Time: 8 ms (28.13%), Space: 6.3 MB (42.77%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    if head == nil {
+        return false
+    }
+
+    slow, fast := head, head
+    for slow.Next != nil && fast.Next != nil && fast.Next.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+
+        if slow == fast {
+            return true
+        }
+    }
+
+    return false
+}
