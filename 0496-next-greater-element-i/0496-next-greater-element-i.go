b974ea380e8bfece8e29b569a83fa07519e9dc2e
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-04-09 22:34:26 +0700

    Time: 0 ms (100%), Space: 5.2 MB (39.94%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.go
+++ 0496-next-greater-element-i/0496-next-greater-element-i.go
@@ -1,26 +1,21 @@
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
-    ans := make([]int, len(nums1))
     m := map[int]int{}
-    for i, v := range nums1 {
-        m[v] = i
+    stack := []int{}
+    for _, v := range nums2 {
+        for len(stack) > 0 && v > stack[len(stack)-1] {
+            m[stack[len(stack)-1]] = v
+            stack = stack[:len(stack)-1]
+        }
+        stack = append(stack, v)
     }
 
-    for i, v := range nums2 {
-        if _, ok := m[v]; !ok {
-            continue   
-        } else {
-            idx := m[v]
-            ans[idx] = -1
-            for j := i + 1; j < len(nums2); j++ {
-                if nums2[j] > nums2[i] {
-                    ans[idx] = nums2[j]
-                    break
-                }
-            }
+    res := make([]int, len(nums1))
+    for i, v := range nums1 {
+        res[i] = -1
+        if val, ok := m[v]; ok {
+            res[i] = val
         }
     }
 
-    return ans
-
-    
+    return res
 }
