7b3019188566dab26d37d33aa7c3b534657c85a5
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-04-11 17:05:44 +0700

    Time: 10 ms (70.34%), Space: 12 MB (38.75%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.go
+++ 0739-daily-temperatures/0739-daily-temperatures.go
@@ -1,12 +1,12 @@
 func dailyTemperatures(temperatures []int) []int {
-    res := make([]int, len(temperatures))
     stack := []int{}
+    res := make([]int, len(temperatures))
     for i, v := range temperatures {
         for len(stack) > 0 && v > temperatures[stack[len(stack)-1]] {
             res[stack[len(stack)-1]] = i - stack[len(stack)-1]
             stack = stack[:len(stack)-1]
         }
-        
+
         stack = append(stack, i)
     }
 
