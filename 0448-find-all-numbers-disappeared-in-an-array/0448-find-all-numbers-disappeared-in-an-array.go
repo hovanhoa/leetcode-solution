cca103a4d41ed1d8c7841da0581278c0663911f3
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-05-30 18:37:16 +0700

    Time: 2 ms (92.14%), Space: 10.2 MB (66.71%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.go
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.go
@@ -1,14 +1,27 @@
-func findDisappearedNumbers(nums []int) []int {
-    seen, ans := map[int]bool{}, []int{}
-    for _, v := range nums {
-        seen[v] = true
-    }
 
-    for i := 1; i <= len(nums); i++ {
-        if !seen[i] {
-            ans = append(ans, i)
+func findDisappearedNumbers(nums []int) []int {
+    // Step 1: Mark visited indices as negative
+    for _, num := range nums {
+        index := abs(num) - 1
+        if nums[index] > 0 {
+            nums[index] = -nums[index]
         }
     }
+    
+    // Step 2: Identify missing numbers by positive elements
+    var result []int
+    for i, num := range nums {
+        if num > 0 {
+            result = append(result, i+1)
+        }
+    }
+    
+    return result
+}
 
-    return ans
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
 }
