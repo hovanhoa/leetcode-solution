7eaf31b2980de85b59439666a2e3138428ccc452
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-06-15 22:47:02 +0700

    Time: 23 ms (52.7%), Space: 9.9 MB (34.05%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -4,30 +4,28 @@ func threeSum(nums []int) [][]int {
     slices.Sort(nums)
     ans := [][]int{}
     i := 0
-    fmt.Println(nums)
     for i <= len(nums) - 3 {
-        left, right := i + 1, len(nums) - 1
-        for left < right {
-            s := nums[i] + nums[left] + nums[right]
+        l, r := i + 1, len(nums) - 1
+        for l < r {
+            s := nums[i] + nums[l] + nums[r]
             if s == 0 {
-                fmt.Println(i, left, right)
-                ans = append(ans, []int{nums[i], nums[left], nums[right]})
-                left += 1
-                for nums[left] == nums[left-1] && left < right {
-                    left += 1
+                ans = append(ans, []int{nums[i], nums[l], nums[r]})
+                l += 1
+                for l < r && nums[l] == nums[l-1] {
+                    l += 1
                 }
             } else if s < 0 {
-                left += 1
+                l += 1
             } else {
-                right -= 1
+                r -= 1
             }
         }
 
         i += 1
-        for i <= len(nums) - 3 && nums[i] == nums[i-1] { 
+        for i <= len(nums) - 3 && nums[i] == nums[i-1] {
             i += 1
         }
     }
 
-    return ans
+    return ans    
 }
