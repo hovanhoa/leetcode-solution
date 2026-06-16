59d5faf455e62cad16784fe92c4332809a0f8ba0
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-06-16 23:10:09 +0700

    Time: 0 ms (100%), Space: 4.2 MB (93.21%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.go
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.go
@@ -1,9 +1,9 @@
 func gcdOfStrings(str1 string, str2 string) string {
     if str1 == str2 {
         return str1
-    }   
+    }
 
-    if len(str2) > len(str1) {
+    if len(str1) < len(str2) {
         return gcdOfStrings(str2, str1)
     }
 
