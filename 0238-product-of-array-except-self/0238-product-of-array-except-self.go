2b683c69a84c6e2cf2e2b73b52f0dabdd4d16cfc
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-06-22 16:34:56 +0700

    Time: 2 ms (65.52%), Space: 9.9 MB (74.81%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -1,19 +1,19 @@
 func productExceptSelf(nums []int) []int {
-    left, right := make([]int, len(nums)), make([]int, len(nums))
-    leftProduct, rightProduct := 1, 1
-    for i, v := range nums {
-        left[i] = leftProduct
-        leftProduct *= v
-    }
-
-    for i := len(nums) - 1; i >= 0; i -- {
-        right[i] = rightProduct
-        rightProduct *= nums[i]
-    }
-
     ans := make([]int, len(nums))
+    for i := range ans {
+        ans[i] = 1
+    }
+
+    prefix := 1
     for i := range nums {
-        ans[i] = left[i] * right[i]
+        ans[i] = prefix
+        prefix *= nums[i]
+    }
+
+    postfix := 1
+    for i := len(nums) - 1; i >= 0; i-- {
+        ans[i] *= postfix
+        postfix *= nums[i]
     }
 
     return ans
