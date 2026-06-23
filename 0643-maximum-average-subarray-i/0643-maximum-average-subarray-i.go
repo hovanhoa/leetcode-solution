04933ba8c7b43ad4b352b3eb14f2edcc96aaf400
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-06-23 22:16:04 +0700

    Time: 0 ms (100%), Space: 10.4 MB (37.53%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.go
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.go
@@ -1,14 +1,14 @@
 func findMaxAverage(nums []int, k int) float64 {
-    s := 0
-    for i := 0; i < k; i ++ {
-        s += nums[i]
+    ans := 0
+    for i := 0; i < k; i++ {
+        ans += nums[i]
     }
 
-    maxSum := s
+    cur := ans
     for i := k; i < len(nums); i++ {
-        s += nums[i] - nums[i-k]
-        maxSum = max(maxSum, s)
+        cur += nums[i] - nums[i-k]
+        ans = max(ans, cur)
     }
 
-    return float64(maxSum)/float64(k)
+    return float64(ans)/float64(k)
 }
