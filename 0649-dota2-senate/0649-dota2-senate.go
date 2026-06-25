6f9167aa08a8ca35e0196661c62d929dfa372d25
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-06-25 16:54:54 +0700

    Time: 2 ms (84.13%), Space: 8.7 MB (19.05%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.go
+++ 0649-dota2-senate/0649-dota2-senate.go
@@ -1,29 +1,26 @@
 func predictPartyVictory(senate string) string {
-    radiants, dires := []int{}, []int{}
-    for i, v := range senate {
-        if v == 'R' {
-            radiants = append(radiants, i)
-        } else {
-            dires = append(dires, i)
-        }
-    }
+	radiants, dires := []int{}, []int{}
+	for i, c := range senate {
+		if c == 'R' {
+			radiants = append(radiants, i)
+		} else {
+			dires = append(dires, i)
+		}
+	}
 
-    for len(radiants) > 0 && len(dires) > 0 {
-        r, d := radiants[0], dires[0]
-        if r < d {
-            radiants = append(radiants, len(senate) + r)
-        } else {
-            dires = append(dires, len(senate) + d)
-        }
+	for len(radiants) > 0 && len(dires) > 0 {
+		r, d := radiants[0], dires[0]
+		radiants, dires = radiants[1:], dires[1:]
+		if r < d {
+			radiants = append(radiants, r+len(senate))
+		} else {
+			dires = append(dires, d+len(senate))
+		}
+	}
 
-        radiants = radiants[1:]
-        dires = dires[1:]
-    }
+	if len(radiants) > 0 {
+		return "Radiant"
+	}
 
-    if len(radiants) > 0 {
-        return "Radiant"
-    }
-
-
-    return "Dire"
+	return "Dire"
 }
