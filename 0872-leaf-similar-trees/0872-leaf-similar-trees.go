1ac9ea2f358cdfcb623598e41b51d93ebef27c1b
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-06-29 12:24:39 +0700

    Time: 0 ms (100%), Space: 4.5 MB (95.73%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.go
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.go
@@ -6,33 +6,34 @@
  *     Right *TreeNode
  * }
  */
-func getLeafs(root *TreeNode, arr *[]int) {
-    if root == nil {
-        return
-    }
-
-    if root.Left == nil && root.Right == nil {
-        *arr = append(*arr, root.Val)
-    }
-
-    getLeafs(root.Left, arr) 
-    getLeafs(root.Right, arr) 
-}
-
 func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
-    arr1, arr2 := make([]int, 0), make([]int, 0)
-    getLeafs(root1, arr1)
-    getLeafs(root2, arr2)
+    arr1, arr2 := []int{}, []int{}
+    getLeafs(root1, &arr1)
+    getLeafs(root2, &arr2)
 
     if len(arr1) != len(arr2) {
         return false
     }
 
-    for i := 0; i < len(arr1); i++ {
+    for i := range arr1 {
         if arr1[i] != arr2[i] {
             return false
         }
     }
 
     return true
+}
+
+func getLeafs(root *TreeNode, arr *[]int) {
+    if root == nil {
+        return 
+    }
+
+    if root.Left == nil && root.Right == nil {
+        *arr = append(*arr, root.Val)
+        return
+    }
+
+    getLeafs(root.Left, arr)
+    getLeafs(root.Right, arr)
 }
