9972546a0006d57980fd75087aa15562ca0fa30f
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-07-03 23:06:40 +0700

    Time: 364 ms (5.95%), Space: 7.9 MB (100%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.go
+++ 0901-online-stock-span/0901-online-stock-span.go
@@ -4,24 +4,23 @@ type StockSpanner struct {
 
 
 func Constructor() StockSpanner {
-    return StockSpanner{
-        stack: []int{},
-    }
+    return StockSpanner{[]int{}}
 }
 
 
 func (this *StockSpanner) Next(price int) int {
-    this.stack = append(this.stack, price)
-    n := 0
-    for i := len(this.stack)-1; i >=0; i-- {
-        if this.stack[i] <= price {
-            n += 1
-        } else {
+    ans := 1
+    for i := len(this.stack) - 1; i >= 0; i-- {
+        if this.stack[i] > price {
             break
         }
+
+        ans++
     }
 
-    return n
+    this.stack = append(this.stack, price)
+
+    return ans
 }
 
 
