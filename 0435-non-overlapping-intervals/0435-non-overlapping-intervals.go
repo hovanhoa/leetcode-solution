b96efabffc9f1bd497b82499dd24e60108b5ecab
hovanhoa <56647826+hovanhoa@users.noreply.github.com>
2026-07-04 14:30:51 +0700

    Time: 27 ms (86.92%), Space: 18.5 MB (46.42%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.go
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.go
@@ -1,16 +1,16 @@
 func eraseOverlapIntervals(intervals [][]int) int {
-    sort.Slice(intervals, func(i, j int) bool {
-        return intervals[i][1] <= intervals[j][1]
+    slices.SortFunc(intervals, func(a, b []int) int {
+        return a[1] - b[1]
     })
 
-    ans := 1
     cur := intervals[0][1]
+    cnt := 1
     for i := 1; i < len(intervals); i++ {
         if intervals[i][0] >= cur {
+            cnt += 1
             cur = intervals[i][1]
-            ans += 1
         }
     }
 
-    return len(intervals) - ans
+    return len(intervals) - cnt
 }
